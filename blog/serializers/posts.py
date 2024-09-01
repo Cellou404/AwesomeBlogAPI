fc6429225be171c938e46531a3cc18a3edc1e596
Cellou <cellou649@gmail.com>
2024-09-01 09:55:18 +0000

    ref: remove comments since the code comprehensible

--- blog/serializers/posts.py
+++ blog/serializers/posts.py
@@ -29,27 +29,17 @@ class PostSerializer(serializers.ModelSerializer):
             "date_updated", "related_posts",
         ]
 
-    """ Get related posts to each post
-    1. Get the current post's id
-    2. Query all other posts that have a similar category to current post
-    3. Remove the current post from the list
-    """
-
     def get_related_posts(self, post):
         category = post.category
         posts = Post.objects.filter(category=category).exclude(id=post.id)
         serializer = BasicPostSerializer(posts, many=True)
         return serializer.data
 
-    # Get comments count for each post
     def get_comments_count(self, post):
         comments = Comment.objects.filter(post=post).count()
         return comments
 
-    # Overrinding default create method
     def create(self, validated_data):
-        author = self.context[
-            "author"
-        ]  # the current  logged-in user is the author while creatind a post
+        author = self.context["author"]
         post = Post.objects.create(author=author, **validated_data)
         return post
